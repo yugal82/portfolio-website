8f3c67507d510ebd3ad8a577bb712a79dbfdd438
Yugal Khanter <yugalkhanter82@gmail.com>
2024-03-07 00:49:18 +0530

    responsiveness in education section

--- src/app/components/Education.jsx
+++ src/app/components/Education.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Education = () => {
     return (
-        <div className='w-full flex items-center justify-between'>
+        <div className='w-full md:flex items-center justify-between'>
             <div>
                 <h2 className='text-xl md:text-3xl font-bold'>Bachelor's of Engineering</h2>
-                <span className='text-lg text-[#adb7b3] font-semibold'>Computer Engineering | Honours: Data Science and Machine Learning</span>
+                <span className='text-base md:text-lg text-[#adb7b3] font-semibold'>Computer Engineering | Honours: Data Science</span>
                 <p className='text-base text-[#adb7b3] font-semibold'>Savitribai Phule Pune University</p>
             </div>
-            <div className='text-[#adb7b3] font-semibold'>
+            <div className=' text-[#adb7b3] font-normal md:font-semibold'>
                 <span className='block'>Jan 2021 - May 2024</span>
-                <span className='block w-full text-right'>8.93 CGPA</span>
+                <span className='block w-full md:text-right'>8.93 CGPA</span>
             </div>
         </div>
     )
